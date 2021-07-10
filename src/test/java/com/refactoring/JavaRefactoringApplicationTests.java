0635ce564d7a0598574ec8d75beadb753e5f2b4f
Ravichandra Reddy <ravichandra.p@outlook.com>
2021-07-10 16:39:45 +0200

    Initial

---
+++ src/test/java/com/refactoring/JavaRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRefactoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
